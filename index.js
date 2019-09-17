82f3865409f9313f98fc6c39c8f7d4a6bf8b9f3b
mgodek <mattigodek@gmail.com>
2019-09-17 12:59:52 -0400

    disable jwt authentication for movies endpoint

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(morgan('common'));
 
 //GET requests
 //all movies
-app.get('/movies', passport.authenticate('jwt', {session: false}), function(req, res) {
+app.get('/movies', function(req, res) {
   Movies.find()
   .then(function(movies) {
     res.status(201).json(movies);
